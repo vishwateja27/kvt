f3913f89a076058d7bc96748279810d04d6f6303
vishwateja27 <39549751+vishwateja27@users.noreply.github.com>
2018-06-04 14:55:48 +0530

    Add files via upload

--- home.ts
+++ home.ts
@@ -1,89 +1,169 @@
-import { Component,ViewChild } from '@angular/core';
-import { NavController,Platform } from 'ionic-angular';
-import {Geolocation} from '@ionic-native/geolocation';
-import {File} from '@ionic-native/file';
-import leaflet, { marker } from 'leaflet';
-
-
-
+import { Component, ViewChild, NgZone} from '@angular/core';
+import { NavController,Platform, LoadingController } from 'ionic-angular';
+import { MediaCapture, MediaFile, CaptureError, CaptureVideoOptions } from '@ionic-native/media-capture';
+import { Storage } from '@ionic/storage';
+import { Media, MediaObject } from '@ionic-native/media';
+import { File } from '@ionic-native/file';
+import { MapPage } from '../map/map';
+import {SecondPage} from '../second/second';
+import { geoJSON } from 'leaflet';
+import {Geolocation,Geoposition} from '@ionic-native/geolocation';
+import { BackgroundGeolocation } from '@ionic-native/background-geolocation';
+ 
+const MEDIA_FILES_KEY = 'mediaFiles';
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController,public file:File,public platform:Platform,public geo:Geolocation) {
-    
-  }
-  @ViewChild('map') mapContainer;
-  options={
-    enableHighAccuracy:true
-  };
-  lat:any;
-  lon:any;
-  flag:any;
-  map:any;
-  obj={
-    LatLon_arr:[]
-  }
+  watch: any;
+  w:any;
+  mediaFiles = [];
+  @ViewChild('myvideo') myVideo: any;
+  lat:any=0;lon:any=0;
   dirPath:any;
-
-  ionViewDidLoad()
-  {
-    this.file.createDir(this.file.externalApplicationStorageDirectory,'finalApp',true).then((dir)=>{
-      alert("directory created"+dir.nativeURL);
-      this.dirPath=dir.toURL;
-
-    }).catch((err)=>{alert("unable to create a dir"+err )});
-    this.map = leaflet.map("map").fitWorld();
-      leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18
-      }).addTo(this.map);
-
-      this.map.locate({
-        setView: true,
-        maxZoom: 10
-      })
-      // .on('locationfound', (e) => {
-      //   let markerGroup = leaflet.featureGroup();
-      //   let marker: any = leaflet.marker([e.latitude, e.longitude]).on('click', () => {
-      //     alert('Marker clicked');
-      //   })
-      //   markerGroup.addLayer(marker);
-      //   this.map.addLayer(markerGroup);
-      //   }).on('locationerror', (err) => {
-      //     alert(err.message);
-      // })
-  }
-
-  startWatch()
+  LatLon_arr=[]
+  flag:any=0;
+  fname:any;
+  constructor(public navCtrl: NavController,public loadingController:LoadingController,public backgroundGeolocation:BackgroundGeolocation,public platform:Platform,public zone:NgZone,private geo:Geolocation ,private mediaCapture: MediaCapture, private storage: Storage, private file: File, private media: Media) 
   {
     
-    let mg={
+  }
+ 
+  ionViewWillEnter() {
+    this.storage.get(MEDIA_FILES_KEY).then(res => {
+    this.mediaFiles = JSON.parse(res) || [];
+    })
+     this.geo.getCurrentPosition({enableHighAccuracy:true});
+     let loader=this.loadingController.create({content:"fetcing location.."});
+     loader.present().then(()=>{
+       this.watch = this.geo.watchPosition({enableHighAccuracy:true,maximumAge:0}).subscribe((position: Geoposition) =>{ 
+         let d:Date=new Date();
+         while(this.lat==0 && this.lon==0)
+         {
+           this.lat = position.coords.latitude;
+           this.lon = position.coords.longitude;
+         }
+         this.lat = position.coords.latitude;
+         this.lon = position.coords.longitude;
+         setTimeout(()=>{
+           this.LatLon_arr.push({lat:this.lat,lon:this.lon,min:d.getMinutes(),sec:d.getSeconds()});        
 
-    };
+         },1000);
+       });
+    loader.dismiss();
+      }); 
+  }
 
-    this.flag=setInterval(()=>{
-      this.geo.getCurrentPosition(this.options).then((res)=>{
-        this.lat=res.coords.latitude;
-        this.lon=res.coords.longitude;
-        this.obj.LatLon_arr.push({lat:res.coords.latitude,lon:res.coords.longitude});
-        if(mg != undefined) 
-          this.map.removeLayer(mg);
-        mg=leaflet.marker([this.lat,this.lon]).addTo(this.map);
+  captureVideo() {
+    this.flag=0;
+    this.lat=0;
+    this.lon=0;
+    let options: CaptureVideoOptions = {
+      limit: 1,
+      duration: 30
+    }
+    
+     this.mediaCapture.captureVideo(options).then((res: MediaFile[]) => {
+      let capturedFile = res[0];
+      let d=new Date(capturedFile.lastModifiedDate);
+      alert(d.getMinutes()+"  "+d.getSeconds())
+      let fileName = capturedFile.name;
+      this.fname=fileName;
+      let dir = capturedFile['localURL'].split('/');
+      dir.pop();
+      let fromDirectory = dir.join('/');      
+      var toDirectory = this.file.dataDirectory;
+      this.file.copyFile(fromDirectory , fileName , toDirectory , fileName).then((res) => {
+      this.storeMediaFiles([{name: fileName, size: capturedFile.size}]);
+//        this.watch.unsubscribe();
+
+      this.stopWatch();        
+      },err => {
+        alert('error: '+ JSON.stringify(err));
+        this.watch.unsubscribe();
       });
-    },1000);
+          },
+    (err: CaptureError) => {alert(JSON.stringify(this.LatLon_arr));console.error(err);alert("error:"+JSON.stringify(err));this.watch.unsubscribe();});   
+          
+  }
+  play(myFile) {
+    if (myFile.name.indexOf('.2wav') > -1) {
+      const audioFile: MediaObject = this.media.create(myFile.localURL);
+      audioFile.play();
+    } else {
+      let path = this.file.dataDirectory + myFile.name;
+      let url = path.replace(/^file:\/\//, '');
+      let video = this.myVideo.nativeElement;
+      video.src = url;
+      video.play();
+
+    }
+  }
+ 
+  storeMediaFiles(files) {
+    this.storage.get(MEDIA_FILES_KEY).then(res => {
+      if (res) {
+        let arr = JSON.parse(res);
+        arr = arr.concat(files);
+        this.storage.set(MEDIA_FILES_KEY, JSON.stringify(arr));
+      } else {
+        this.storage.set(MEDIA_FILES_KEY, JSON.stringify(files))
+      }
+      this.mediaFiles = this.mediaFiles.concat(files);
+    })
+  }
+
+  mappage()
+  {
+    this.navCtrl.push(MapPage);
+  }
+  startWatch()
+  { 
+    this.lat=0;this.lon=0;   
+    let loader=this.loadingController.create({content:"fetcing location.."});
+     loader.present().then(()=>{
+       this.watch = this.geo.watchPosition({enableHighAccuracy:true,maximumAge:0}).subscribe((position: Geoposition) =>{ 
+         let d:Date=new Date();
+         while(this.lat==0 && this.lon==0)
+         {
+           this.lat = position.coords.latitude;
+           this.lon = position.coords.longitude;
+         }
+         this.lat = position.coords.latitude;
+         this.lon = position.coords.longitude;
+         setTimeout(()=>{
+           this.LatLon_arr.push({lat:this.lat,lon:this.lon,min:d.getMinutes(),sec:d.getSeconds()});        
+
+         },1000);
+       });
+    loader.dismiss();
+      }); 
   }
   stopWatch()
-  {
-    let jObj=JSON.stringify(this.obj);
-    let fop=this.file.writeFile(this.dirPath,'LatLon.json',jObj,{append:true,replace:false});
-    fop.then((fData)=>{
-      alert("Fle created at:"+this.dirPath)
-    }).catch(error=>{
-      alert("unable to create a file :"+error);
-    });
-    clearInterval(this.flag);
+  { 
+    let jObj=JSON.stringify(this.LatLon_arr);
+    alert(jObj);
+    // this.file.createFile(this.file.externalApplicationStorageDirectory,'fname.json',true).then(()=>{ 
+    // }).catch(err=>{
+    //   alert("error creating a file");
+    // });
+    //let f=this.fname.append('.json');
+    //this.file.writeFile(this.file.externalApplicationStorageDirectory,"fname.json",JSON.stringify(this.LatLon_arr),{replace:true}).then(()=>{
+      //alert("contents written succcesfully");
+      // this.file.readAsText(this.file.externalApplicationStorageDirectory,'fname.json').then(fData=>{
+      //   alert("contents are:"+fData);
+      // }).catch(err=>{
+      //   alert("error reading the comntentrs of the files");
+      // })
+    // }).catch(err=>{
+    //   alert("error writing into file: "+JSON.stringify(err));
+    // }); 
+    this.empty();
   }
-
+  empty()
+  {
+    this.LatLon_arr.length=0;
+  }
 }
